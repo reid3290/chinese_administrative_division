4903cb1ed49ec5468f89007ed874b8bab9f1a831
Jianhu <geniusreid@163.com>
2017-02-06 14:19:24 +0800

    增加省、市、区的独立数据

--- src/index.js
+++ src/index.js
@@ -29,24 +29,59 @@ request(XZQH_URL, (error, response, body) => {
         const res = {};
         let currentProvince = '',
             currentCity = '';
+        const provinces = [];
+        const cities = [];
+        const districts = [];
         data.forEach(item => {
             const a = item.split(' ');
             if (a[0].substr(2, 4) === '0000') {
                 // 省份数据
                 currentProvince = a[1];
+                provinces.push({
+                    code: a[0],
+                    name: a[1]
+                });
                 res[currentProvince] = {};
             }
             else if (a[0].substr(4, 2) === '00' && a[0].substr(2, 4) !== '0000') {
                 // 城市数据
                 currentCity = a[1];
+                cities.push({
+                    code: a[0],
+                    name: a[1],
+                    parent_code:  `${a[0].substr(0, 2)}0000`
+                });
                 res[currentProvince][currentCity] = [];
             }
             else if (a[0].substr(4, 2) !== '00') {
                 // 区县数据
+                districts.push({
+                    code: a[0],
+                    name: a[1],
+                    parent_code:  `${a[0].substr(0, 4)}00`
+                });
                 res[currentProvince][currentCity].push(a[1]);
             }
         });
         fs.writeFile('../dist/province_city_district.json', JSON.stringify(res), (error)=>{
+            if(error) {
+                console.error(error);
+                return;
+            }
+        });
+        fs.writeFile('../dist/provinces.json', JSON.stringify(provinces), (error)=>{
+            if(error) {
+                console.error(error);
+                return;
+            }
+        });
+        fs.writeFile('../dist/cities.json', JSON.stringify(cities), (error)=>{
+            if(error) {
+                console.error(error);
+                return;
+            }
+        });
+        fs.writeFile('../dist/districts.json', JSON.stringify(districts), (error)=>{
             if(error) {
                 console.error(error);
                 return;
